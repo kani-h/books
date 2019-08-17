d997153b712963d0a40deadff078cdbc0091b00b
kani <kani@kanistyle.com>
2019-08-17 21:03:53 +0900

    ch02 section1 complete

---
+++ kotlin/do it kotlin programing/KotilinProgramming/src/chap02/section1/defaultPackage.kt
@@ -0,0 +1,15 @@
+package chap02.section1
+
+import kotlin.math.PI
+import kotlin.math.abs
+
+fun main() {
+
+	val intro: String = "안녕하십니까?"
+	val num: Int = 20
+
+	println(PI)
+	println(abs(-12.26))
+
+	println("intro: $intro, num $num")
+}
